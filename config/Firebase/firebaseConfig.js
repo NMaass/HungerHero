7a9ff83574c13bf1f74b63ab4807b83f348be41a
NMaass <Nicholasmaassen@yahoo.com>
2020-02-01 21:52:30 -0500

    Add files via upload

---
+++ config/Firebase/firebaseConfig.js
@@ -0,0 +1,12 @@
+// Rename this file to "firebaeConfig.js" before use
+// Replace all Xs with real Firebase API keys
+
+export default {
+  apiKey: 'AIzaSyDpT6prnIuoHT6waMcSYIVEkGFhba1w8zg',
+  authDomain: '594352546689-lnoiig2j7i53jalb56lpvlljincjak7m.apps.googleusercontent.com',
+  databaseURL: 'https://hungerhero-42be4.firebaseio.com',
+  projectId: 'hungerhero-42be4',
+  storageBucket: 'hungerhero-42be4.appspot.com',
+  messagingSenderId: '594352546689',
+  appId: '1:594352546689:android:6ae91f914f54b1c5d03e80'
+}
