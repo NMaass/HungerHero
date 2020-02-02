b07ccedc544beae603cc860ccf57b3a4ce641ba9
Nicholas Maassen <nicholasfmaassen@gmail.com>
2020-02-01 21:38:41 -0500

    adds firebase integration

--- config/Firebase/example.firebaseConfig.js
+++ config/Firebase/example.firebaseConfig.js
@@ -2,11 +2,11 @@
 // Replace all Xs with real Firebase API keys
 
 export default {
-  apiKey: 'XXXX',
-  authDomain: 'XXXX',
-  databaseURL: 'XXXX',
-  projectId: 'XXXX',
-  storageBucket: 'XXXX',
-  messagingSenderId: 'XXXX',
-  appId: 'XXXX'
+  apiKey: 'AIzaSyDpT6prnIuoHT6waMcSYIVEkGFhba1w8zg',
+  authDomain: '594352546689-lnoiig2j7i53jalb56lpvlljincjak7m.apps.googleusercontent.com',
+  databaseURL: 'https://hungerhero-42be4.firebaseio.com',
+  projectId: 'hungerhero-42be4',
+  storageBucket: 'hungerhero-42be4.appspot.com',
+  messagingSenderId: '1:594352546689:android:6ae91f914f54b1c5d03e80',
+  appId: '1:594352546689:android:6ae91f914f54b1c5d03e80'
 }
